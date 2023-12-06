cd97b336b2af8a2faf4830fa9478061d95ea22dc
André Luiz <124028192+AndreLuizAlmeida@users.noreply.github.com>
2023-12-06 07:33:01 -0300

    Add files via upload

---
+++ AntecessorSucessor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AntecessorSucessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"O antecessor de {num} é {num-1} e seu sucessor é {num+1}");
+        }
+    }
+} 
