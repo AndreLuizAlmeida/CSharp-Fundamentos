ae6df66b4b6617f1c17c45c1a0b9b38080dcf52e
André Luiz <124028192+AndreLuizAlmeida@users.noreply.github.com>
2023-12-06 07:39:09 -0300

    Add files via upload

---
+++ IMC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace IMC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o valor do seu peso: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+            
+            Console.WriteLine("Insira o valor do sua altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double imc = peso / (altura * altura);
+            Console.WriteLine(imc);
+            if (imc <= 18.5)
+            {
+                Console.WriteLine("Abaixo do peso");
+            } 
+            else if (imc > 18.5 && imc <= 24.9)
+            {
+                Console.WriteLine("Peso ideal");
+            }
+            else if (imc > 25 && imc <= 29.9)
+            {
+                Console.WriteLine("Acima do peso");
+            }
+            else if (imc > 30 && imc <= 34.9)
+            {
+                Console.WriteLine("Obesidade Grau 1");
+            }
+            else if (imc > 35 && imc <= 39.9)
+            {
+                Console.WriteLine("Obesidade Grau 2");
+            }
+            else
+            {
+                Console.WriteLine("Obesidade Grau 3");
+            }
+        }
+    }
+}
---
+++ MaiorEMenor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MaiorEMenor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o primeiro numero: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o segundo numero: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o terceiro numero: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            int maiorValor = 0;
+            int menorValor = 0;
+            //Definindo maior valor
+            if (num1 > num2 && num1 > num3)
+            {
+                maiorValor = num1;
+            }
+            else if (num2 > num1 && num2 > num3)
+            {
+                maiorValor = num2;
+            }
+            else
+            {
+                maiorValor = num3;
+            }
+            //Definindo menor valor
+            if (num1 < num2 && num1 < num3)
+            {
+                menorValor = num1;
+            }
+            else if (num2 < num1 && num2 < num3)
+            {
+                menorValor = num2;
+            }
+            else
+            {
+                menorValor = num3;
+            }
+            Console.WriteLine($"Entre os números {num1},{num2},{num3} o maior é {maiorValor}");
+            Console.WriteLine($"Entre os números {num1},{num2},{num3} o menor é {menorValor}");
+        }
+    }
+}
