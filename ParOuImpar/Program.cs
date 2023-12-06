ffb71a2ce22d16a43c4e6d935a6f2fe9f1ee33f7
André Luiz <124028192+AndreLuizAlmeida@users.noreply.github.com>
2023-12-06 07:45:11 -0300

    Add files via upload

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um valor: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0)
+            {
+                Console.WriteLine($"O número {num} é par");
+            }
+            else
+            {
+                Console.WriteLine($"O número {num} é ímpar");
+            }
+        }
+    }
+}
