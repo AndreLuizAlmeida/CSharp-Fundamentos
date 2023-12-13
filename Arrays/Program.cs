1c4238d9a4df85fa3179c6f3b2bc6d88699be755
André Luiz <124028192+AndreLuizAlmeida@users.noreply.github.com>
2023-12-13 11:19:30 -0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meuArray = new int[5]{1,2,3,4,5};
+
+            // for (int i = 0; i < meuArray.Length; i++)
+            // {
+            //     Console.Write(meuArray[i] + " ");
+            //     
+            // }
+            // foreach (var i in meuArray)
+            // {
+            //     Console.WriteLine(i);
+            // }
+            var funcionarios = new Funcionario[5];
+
+            foreach (var funcionario in funcionarios)
+            {
+                Console.WriteLine(funcionario.Id);
+            }
+        }
+    }
+}
+
+public struct Funcionario
+{
+    public int Id { get; set; }
+}
